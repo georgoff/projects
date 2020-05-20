13e81f9839cfe748ff337601fdf5dbc6d94d0c91
Alec Georgoff <georgoff@uw.edu>
2020-05-19 22:02:21 -0700

    Added and tested more API functions.

--- python/spotify/api_calls.py
+++ python/spotify/api_calls.py
@@ -8,8 +8,15 @@ Created on Mon May 18 21:28:49 2020
 
 import requests
 
+
+
+### USER TASTE ###
+# Endpoints for retrieving information about the user’s listening habits.
+
 def GetTopSomething(typeToGet, userToken, limit = '20', offset = '0', timeRange = 'medium_term'):
-    # Requires user token and "user-top-read" scope
+    # Get the current user’s top artists or tracks based on calculated affinity.
+    
+    # Requires user token from authorization service and "user-top-read" scope
     # typeToGet: "artists" or "tracks"
     # limit: (optional) The number of entries to return. Min = 1, Max = 50
     # offset: (optional) The index of the first entity to return
@@ -21,9 +28,156 @@ def GetTopSomething(typeToGet, userToken, limit = '20', offset = '0', timeRange 
     address = f'https://api.spotify.com/v1/me/top/{typeToGet}'
     headers = {'Authorization':f'Bearer {userToken}'}
     payload = {
-        'limit':limit,
-        'offset':offset,
+        'limit':str(limit),
+        'offset':str(offset),
         'time_range':timeRange
+        }
+    
+    r = requests.get(address, headers=headers, params=payload)
+    
+    return r.json()
+
+
+
+### TRACKS ###
+# Endpoints for retrieving information about one or more tracks from the Spotify catalog.
+
+def GetTrack(userToken, trackId):
+    # Get Spotify catalog information for a single track identified by its unique Spotify ID.
+    
+    # Requires user token from authorization service (no scope)
+    # trackId: The Spotify ID for the desired track
+    # There is an optional "market" query parameter (not implemented here)
+    # that can be used for track relinking
+    
+    address = f'https://api.spotify.com/v1/tracks/{trackId}'
+    headers = {'Authorization':f'Bearer {userToken}'}
+    
+    r = requests.get(address, headers=headers)
+    
+    return r.json()
+
+def GetSeveralTracks(userToken, trackIds):
+    # Get Spotify catalog information for multiple tracks based on their Spotify IDs.
+    
+    # Requires user token from authorization service (no scope)
+    # trackIds: A list of Spotify IDs for the desired tracks (max of 50)
+    # There is an optional "market" query parameter (not implemented here)
+    # that can be used for track relinking
+    
+    parameterString = '?ids='
+    
+    for track in trackIds:
+        parameterString += f'{track},'
+    
+    address = 'https://api.spotify.com/v1/tracks/' + parameterString.strip(',')
+    headers = {'Authorization':f'Bearer {userToken}'}
+    
+    r = requests.get(address, headers=headers)
+    
+    return r.json()
+
+def GetTrackAudioFeatures(userToken, trackId):
+    # Get audio feature information for a single track identified by its unique Spotify ID.
+    
+    # Requires user token from authorization service (no scope)
+    # trackId: The Spotify ID for the desired track
+    
+    address = f'https://api.spotify.com/v1/audio-features/{trackId}'
+    headers = {'Authorization':f'Bearer {userToken}'}
+    
+    r = requests.get(address, headers=headers)
+    
+    return r.json()
+
+def GetMultipleTrackAudioFeatures(userToken, trackIds):
+    # Get audio features for multiple tracks based on their Spotify IDs.
+    
+    # Requires user token from authorization service (no scope)
+    # trackIds: A list of Spotify IDs for the desired tracks (max 100)
+    
+    parameterString = '?ids='
+    
+    for track in trackIds:
+        parameterString += f'{track},'
+    
+    address = 'https://api.spotify.com/v1/audio-features/' + parameterString.strip(',')
+    headers = {'Authorization':f'Bearer {userToken}'}
+    
+    r = requests.get(address, headers=headers)
+    
+    return r.json()
+
+def GetTrackAudioAnalysis(userToken, trackId):
+    # Get a detailed audio analysis for a single track identified by its unique Spotify ID.
+    
+    # Requires user token from the authorization service (no scope)
+    # trackId: The Spotify ID for the desired track
+    
+    address = f'https://api.spotify.com/v1/audio-analysis/{trackId}'
+    headers = {'Authorization':f'Bearer {userToken}'}
+    
+    r = requests.get(address, headers=headers)
+    
+    return r.json()
+
+
+
+### BROWSE ###
+# Endpoints for getting playlists and new album releases featured on Spotify’s Browse tab.
+
+def GetCategory(userToken, categoryId):
+    # Get a single category used to tag items in Spotify (on, for example, the Spotify player’s “Browse” tab).
+    
+    # Requires user token from authorization service (no scope)
+    # categoryId: The Spotify Category ID for the desired category
+    # There are optional "country" and "locale" query parameters
+    # (not implemented here) that control country availability and
+    # results language
+    
+    address = f'https://api.spotify.com/v1/browse/categories/{categoryId}'
+    headers = {'Authorization':f'Bearer {userToken}'}
+    
+    r = requests.get(address, headers=headers)
+    
+    return r.json()
+
+def GetCategoryPlaylists(userToken, categoryId, limit = 20, offset = 0):
+    # Get a list of Spotify playlists tagged with a particular category.
+    
+    # Requires user token from authorization service (no scope)
+    # categoryId: The Spotify Category ID for the desired category
+    # limit: (optional) The number of entries to return. Min = 1, Max = 50
+    # offset: (optional) The index of the first entity to return
+    # There is an optional "country" query parameter (not implemented here)
+    # that controls the country result
+    
+    address = f'https://api.spotify.com/v1/browse/categories/{categoryId}/playlists'
+    headers = {'Authorization':f'Bearer {userToken}'}
+    payload = {
+        'limit':str(limit),
+        'offset':str(offset)
+        }
+    
+    r = requests.get(address, headers=headers, params=payload)
+    
+    return r.json()
+
+def GetListOfCategories(userToken, limit = 20, offset = 0):
+    # Get a list of categories used to tag items in Spotify (on, for example, the Spotify player’s “Browse” tab).
+    
+    # Requires user token from authorization service (no scope)
+    # limit: (optional) The number of entries to return. Min = 1, Max = 50
+    # offset: (optional) The index of the first entity to return
+    # There are optional "country" and "locale" query parameters
+    # (not implemented here) that control country availability and
+    # results language
+    
+    address = f'https://api.spotify.com/v1/browse/categories'
+    headers = {'Authorization':f'Bearer {userToken}'}
+    payload = {
+        'limit':str(limit),
+        'offset':str(offset)
         }
     
     r = requests.get(address, headers=headers, params=payload)
--- python/spotify/top_songs.py
+++ python/spotify/top_songs.py
@@ -6,13 +6,15 @@ Created on Sat May 16 22:41:00 2020
 @author: georgoff
 """
 
-from api_calls import GetTopSomething
+from api_calls import *
 from authorization import GetToken
 from user_secrets import clientId, clientSecret, refreshToken
 
 token, expiresIn, refreshToken, scope = GetToken(refreshToken, clientId, clientSecret, refresh = True)
 
-myTopSongs = GetTopSomething('tracks', token, limit = '35', timeRange = 'short_term')
+# myTopSongs = GetTopSomething('tracks', token, limit = '35', timeRange = 'short_term')
 
-for i in myTopSongs['items']:
-    print(i['name'])
+# for i in myTopSongs['items']:
+#     print(i['name'])
+
+sampleCategories = GetListOfCategories(token)
